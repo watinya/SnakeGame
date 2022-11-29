ca2a4540a178594676f7f11a23b50916ef9a2821
watinya <63241680+watinya@users.noreply.github.com>
2022-11-29 22:34:57 +0800

    修404

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ const startBoard = document.getElementById('start-board')
 function main(currentTime) {
     if (gameOver) {
         if(confirm('You lost!! Press ok to restart')) {
-            window.location = '/'
+            window.location.reload();
         }
         return
     }
